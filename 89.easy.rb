b27d146a1f37466ce8a6ed8fce532bc81c2d4e35
Bryan Schlief <bschlief@gmail.com>
2012-08-21 18:33:48 -0700

    Fix whitespace

--- 89.easy.rb
+++ 89.easy.rb
@@ -18,28 +18,28 @@
 # Retrieve from http://www.reddit.com/r/dailyprogrammer/comments/y5sox/8132012_challenge_88_easy_vigen%C3%A8re_cipher/ 
 # on 8-20-2012
 
-    class Array 
-      def sum 
-        self.inject(:+) 
-      end
+class Array 
+  def sum 
+    self.inject(:+) 
+  end
 
-      def mean 
-        self.sum / self.size 
-      end
+  def mean 
+    self.sum / self.size 
+  end
 
-      def variance
-        mu = self.mean
-        (self.inject(0) { |res,n| res += (n-mu)** 2 })/self.size
-      end 
+  def variance
+    mu = self.mean
+    (self.inject(0) { |res,n| res += (n-mu)** 2 })/self.size
+  end 
 
-      def std_dev 
-        self.variance ** 0.5 
-      end
-    end
+  def std_dev 
+    self.variance ** 0.5 
+  end
+end
 
-    vals = IO.readlines("data/89.easy.input.txt").map { |line| line.to_f }
+vals = IO.readlines("data/89.easy.input.txt").map { |line| line.to_f }
 
-    puts "Mean:\t\t#{vals.mean}"
-    puts "Variance:\t#{vals.variance}"
-    puts "Std Dev:\t#{vals.std_dev}"
+puts "Mean:\t\t#{vals.mean}"
+puts "Variance:\t#{vals.variance}"
+puts "Std Dev:\t#{vals.std_dev}"
 
